65d1053afde8db90d2bda5d91b43e3e3b6911d8a
Ágatha <agathamagnusg@hotmail.com>
2024-06-19 10:22:29 -0300

    ✨ feat: add basic structure for event entity in domain package
    - Define validation errors for Event;
    - Create Rating type with constants;
    - Define Event struct with fields;
    - Implement Validate function for Spot struct to ensure valid spot

---
+++ internal/events/domain/spot.go
@@ -0,0 +1,48 @@
+package domain
+
+import "errors"
+
+var (
+	ErrInvalidSpotNumber = errors.New("spot number is invalid")
+	ErrSpotNotFound = errors.New("spot not found")
+	ErrSpotAlreadyReserved = errors.New("spot is already reserved")
+	ErrSpotNameRequired = errors.New("spot name is required")
+	ErrSpotNameTwoCharacters = errors.New("spot name must be at least 2 characters long")
+	ErrSpotNameStartWithLetter = errors.New("spot name must start with a letter")
+	ErrSpotNameEndWithNumber = errors.New("spot name must end with a number")
+)
+
+type SpotStatus string
+
+const (
+	SpotStatusAvailable SpotStatus = "available"
+	SpotStatusSold      SpotStatus = "sold"
+)
+
+type Spot struct {
+	ID       string
+	EventID  string
+	Name     string
+	Status   SpotStatus
+	TicketId string
+}
+
+func (s *Spot) Validate() error {
+	if len(s.Name) == 0 {
+		return ErrSpotNameRequired
+	}
+
+	if len(s.Name) < 2 {
+		return ErrSpotNameTwoCharacters
+	}
+
+	if s.Name[0] < 'A' || s.Name[0] > 'Z' {
+		return ErrSpotNameStartWithLetter
+	}
+
+	if s.Name[1] < '0' || s.Name[1] > '9' {
+		return ErrSpotNameEndWithNumber
+	}
+
+	return nil
+}
