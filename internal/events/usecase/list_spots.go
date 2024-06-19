4320fe7cb2f080c904de107650472791e5174ea1
Ágatha <agathamagnusg@hotmail.com>
2024-06-19 17:16:03 -0300

    ✨ feat: add usecase list spots

---
+++ internal/events/usecase/list_spots.go
@@ -0,0 +1,60 @@
+package usecase
+
+import "github.com/htamagnus/ticket-sales-backend-golang/internal/events/domain"
+
+type ListSpotsInputDTO struct {
+	EventID string `json:"event_id"`
+}
+
+type ListSpotsOutputDTO struct {
+	Spots []SpotDTO `json:"spots"`
+	Event EventDTO `json:"event"`
+}
+
+type ListSpotsUseCase struct {
+	repo domain.EventRepository
+}
+
+func NewListSpotsUseCase(repo domain.EventRepository) *ListSpotsUseCase {
+	return &ListSpotsUseCase{repo: repo}
+}
+
+func (uc *ListSpotsUseCase) Execute(input ListSpotsInputDTO) (*ListSpotsOutputDTO, error) {
+	event, err := uc.repo.FindEventByID(input.EventID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	spots, err := uc.repo.FindSpotsByEventID(input.EventID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	spotDTOs := make([]SpotDTO, len(spots))
+
+	for i, spot := range spots {
+		spotDTOs[i] = SpotDTO{
+			ID:       spot.ID,
+			Name:     spot.Name,
+			Status:   string(spot.Status),
+			TicketId: spot.TicketId,
+		}
+	}
+
+	eventDTO := EventDTO{
+		ID:           event.ID,
+		Name:         event.Name,
+		Location:     event.Location,
+		Organization: event.Organization,
+		Rating:       string(event.Rating),
+		Date:         event.Date.Format("2006-01-02 15:04:05"),
+		ImageURL:     event.ImageURL,
+		Capacity:     event.Capacity,
+		Price:        event.Price,
+		PartnerID:    event.PartnerID,
+}
+
+	return &ListSpotsOutputDTO{Spots: spotDTOs, Event: eventDTO}, nil
+}
