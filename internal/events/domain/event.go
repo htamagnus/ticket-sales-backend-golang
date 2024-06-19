96d356270187f21e320abea9a712a1807c2429b6
Ágatha <agathamagnusg@hotmail.com>
2024-06-19 13:38:24 -0300

    ✨ feat: add func add stop to event

--- internal/events/domain/event.go
+++ internal/events/domain/event.go
@@ -58,3 +58,13 @@ func (e Event) Validate() error {
 	return nil
 }
 
+func (e *Event) AddSpot(name string) (*Spot, error) {
+	spot, err := NewSpot(e, name)
+	if err != nil {
+		return nil, err
+	}
+
+	e.Spots = append(e.Spots, *spot)
+
+	return spot, nil
+}
