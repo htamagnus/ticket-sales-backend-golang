59a280b5c38e55680e49eed105079daac40dae16
Ágatha <agathamagnusg@hotmail.com>
2024-06-21 20:04:50 -0300

    ✨ update ticket in domain

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,18 +1,30 @@
 package domain
 
-import "errors"
+import (
+	"errors"
 
+	"github.com/google/uuid"
+)
+
+// Errors
+var (
+	ErrInvalidTicketType = errors.New("invalid ticket type")
+)
+
+// TicketType represents the type of a ticket.
 type TicketType string
 
-var (
-	ErrTicketPriceZero = errors.New("ticket price must be greater than zero")
-)
-
 const (
-	TicketTypeHalf TicketType = "half"
-	TicketTypeFull TicketType = "full"
+	TicketTypeHalf TicketType = "half" // Half-price ticket
+	TicketTypeFull TicketType = "full" // Full-price ticket
 )
 
+// IsValidTicketType checks if a ticket type is valid.
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+// Ticket represents a ticket for an event.
 type Ticket struct {
 	ID         string
 	EventID    string
@@ -21,20 +33,37 @@ type Ticket struct {
 	Price      float64
 }
 
-func IsValidTicketType(ticketType TicketType) bool {
-	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+// NewTicket creates a new ticket with the given parameters.
+func NewTicket(event *Event, spot *Spot, ticketType TicketType) (*Ticket, error) {
+	if !IsValidTicketType(ticketType) {
+		return nil, ErrInvalidTicketType
+	}
+
+	ticket := &Ticket{
+		ID:         uuid.New().String(),
+		EventID:    event.ID,
+		Spot:       spot,
+		TicketType: ticketType,
+		Price:      event.Price,
+	}
+	ticket.CalculatePrice()
+	if err := ticket.Validate(); err != nil {
+		return nil, err
+	}
+	return ticket, nil
 }
 
+// CalculatePrice calculates the price based on the ticket type.
 func (t *Ticket) CalculatePrice() {
 	if t.TicketType == TicketTypeHalf {
-		t.Price /= 2 
+		t.Price /= 2
 	}
 }
 
+// Validate checks if the ticket data is valid.
 func (t *Ticket) Validate() error {
 	if t.Price <= 0 {
-		return ErrTicketPriceZero
+		return errors.New("ticket price must be greater than zero")
 	}
-
 	return nil
 }
